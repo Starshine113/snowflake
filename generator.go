9bb95328b64694373d4cea9c72b1a4e529063045
Starshine System <us@starshines.xyz>
2021-01-29 11:42:42 +0100

    change Snowflake type to uint64

--- generator.go
+++ generator.go
@@ -1,9 +1,7 @@
 package snowflake
 
 import (
-	"fmt"
 	"math/rand"
-	"strconv"
 	"sync"
 	"time"
 )
@@ -11,7 +9,7 @@ import (
 // Generator holds info needed for generating custom snowflakes
 type Generator struct {
 	Epoch     time.Time
-	increment uint64
+	increment uint16
 	rand      *rand.Rand
 	mu        sync.Mutex
 }
@@ -30,15 +28,22 @@ func (g *Generator) Get() Snowflake {
 	id |= (g.getRandom() << 16)
 	id |= g.getIncrement()
 
-	return Snowflake(fmt.Sprintf("%v", id))
+	return Snowflake(id)
+}
+
+// GetFromTime gets a snowflake, with only the time field set, that parses as the given time
+func (g *Generator) GetFromTime(t time.Time) Snowflake {
+	if g.Epoch.After(t) {
+		return 0
+	}
+
+	d := t.Sub(g.Epoch).Milliseconds()
+	return Snowflake(uint64(d) << 22)
 }
 
 // Parse parses the given snowflake into a timestamp
 func (g *Generator) Parse(s Snowflake) (t time.Time, err error) {
-	i, err := strconv.ParseInt(string(s), 10, 64)
-	if err != nil {
-		return
-	}
+	i := int64(s)
 
 	timestamp := (i >> 22) + int64(g.Epoch.UnixNano()/1000000)
 	t = time.Unix(0, timestamp*1000000).UTC()
@@ -47,7 +52,7 @@ func (g *Generator) Parse(s Snowflake) (t time.Time, err error) {
 
 func (g *Generator) getIncrement() uint64 {
 	g.mu.Lock()
-	inc := g.increment
+	inc := uint64(g.increment)
 	g.increment++
 	g.mu.Unlock()
 	return inc
